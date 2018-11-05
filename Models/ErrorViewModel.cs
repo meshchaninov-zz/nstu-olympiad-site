1ae2197ba19a282645dcb48d37aea1318a453c8a
Nikita Meshchaninov <nikita@MacBook-Pro-Nikita.local>
2018-11-05 13:43:12 +0700

    Initall commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace nstu_olympiad_site.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
